76764330f2526b8e1baf14e18c066f698cc9fa24
WalleyemanZ <jacobzea96@gmail.com>
2023-04-01 11:34:46 -0600

    finished product-routes.js

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -21,9 +21,17 @@ router.get('/', (req, res) => {
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  try {
+    const product = await Product.findByPk(req.params.id, {
+      include: [{ model: Category }, { model: Tag, through: ProductTag }],
+    });
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
 // create new product
@@ -100,8 +108,22 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
+  try {
+    const product = await Product.destroy({
+      where: {
+        id: req.params.id,
+      }
+    });
+    if (!product) {
+      res.status(404).json({ message: 'No product found with this id!' });
+      return;
+    }
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
 module.exports = router;
